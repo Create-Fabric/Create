7532bc640c5a0b7f7c4dfe58c9f7ebdcdefac7a4
Yjn024 <61787533+JieningYu@users.noreply.github.com>
2022-01-25 19:05:48 +0800

    Add files via upload

---
+++ src/main/java/com/simibubi/create/compat/rei/display/FanHauntingDisplay.java
@@ -0,0 +1,9 @@
+package com.simibubi.create.compat.rei.display;
+
+import com.simibubi.create.content.contraptions.components.fan.HauntingRecipe;
+
+public class FanHauntingDisplay extends AbstractCreateDisplay<HauntingRecipe> {
+	public FanHauntingDisplay(HauntingRecipe recipe) {
+		super(recipe, "fan_haunting");
+	}
+}
